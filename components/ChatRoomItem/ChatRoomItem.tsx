0a324447ab73293cb6ca21f6335ab57e85486edf
Renato Ferreira de Assis <renato.assis@g.globo>
2022-03-05 22:22:01 -0300

    add: navigation in the chatroom

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Text, Image, View } from 'react-native'
+import { useNavigation } from "@react-navigation/native";
+import { Text, Image, View, Pressable } from 'react-native'
 
 import styles from './styles'
 
 const ChatRoomItem = ({ chatRoom }) => {
   const user = chatRoom.users[1]
 
+  const navigation = useNavigation()
+
+  const onPress = () => {
+    navigation.navigate('ChatRoom', { id: chatRoom.id })
+  }
+
   return (
-    <View style={styles.container}>
+    <Pressable onPress={onPress} style={styles.container}>
       <Image style={styles.image} source={{ uri: user.imageUri }} />
 
       {chatRoom.newMessages && <View style={styles.badgeContainer}>
@@ -21,7 +28,7 @@ const ChatRoomItem = ({ chatRoom }) => {
         </View>
         <Text numberOfLines={1} ellipsizeMode='tail' style={styles.text}>{chatRoom.lastMessage.content}</Text>
       </View>
-    </View>
+    </Pressable>
   )
 }
 
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Text, View, StyleSheet, FlatList, SafeAreaView } from 'react-native'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import chatRoomsData from '../assets/dummy-data/ChatRooms'
 
 import Message from '../components/Message/Message';
 import chatRoomData from '../assets/dummy-data/Chats'
 import MessageInput from '../components/MessageInput/MessageInput';
 
 const ChatRoomScreen = () => {
+  const route = useRoute()
+  const navigation = useNavigation()
+
+  navigation.setOptions({ title: 'Elon Musk' })
+
   return (
     <SafeAreaView style={styles.page}>
       <FlatList
