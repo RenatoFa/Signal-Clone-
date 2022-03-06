7bc5ac8e02b863d1cab633495907ae70232927ce
Renato Ferreira de Assis <renato.assis@g.globo>
2022-03-06 01:39:04 -0300

    feat: add authenticator

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -24,3 +25,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App)
